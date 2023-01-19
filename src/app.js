bee5759a7db2494fcf7255c505dadca4a601c756
alebig <alebig.pciencia@gmail.com>
2023-01-19 11:35:46 -0300

    en app.js reemplazado res.header por res.set

--- src/app.js
+++ src/app.js
@@ -14,8 +14,8 @@ app.use(express.json());
 
 // Agregado para evitar bloqueo CORS al frontend
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.set("Access-Control-Allow-Origin", "*");
+//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 
