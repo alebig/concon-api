39d0e6e3fc27e2619c32a48ebaf9a7522ff693ff
alebig <alebig.pciencia@gmail.com>
2023-06-05 14:29:49 -0300

    Quité de config.js DB_PORT 4038

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import { config } from 'dotenv';
 config();
 
 //    export const PORT = process.env.DB_HOST || 4038;
-    export const DB_PORT = process.env.DB_PORT || 4038;
+//    export const DB_PORT = process.env.DB_PORT || 4038;
     export const DB_HOST = process.env.DB_HOST || '191.101.235.205';
     export const DB_DATABASE = process.env.DB_DATABASE || 'concon';
     export const DB_USER = process.env.DB_USER || 'alebig';
