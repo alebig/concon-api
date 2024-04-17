faa1b10f5e7159dc57a063ed477602f821cec09e
alebig <alebig.pciencia@gmail.com>
2024-04-17 19:05:53 -0300

    .env diferenciados

--- src/config.js
+++ src/config.js
@@ -2,19 +2,9 @@ import { config } from 'dotenv';
 
 config();
 
-    export const PORT = process.env.DB_HOST // 4038;
-    export const DB_PORT = process.env.DB_PORT // 3306;
-    export const DB_HOST = process.env.DB_HOST // localhost/'191.101.235.205';
-    export const DB_DATABASE = process.env.DB_DATABASE // 'concon';
-    export const DB_USER = process.env.DB_USER // 'alebig';
-    export const DB_PASSWORD = process.env.DB_PASSWORD // 'jab3655';
-
-
-//console.log(configu);
-
-/* export default {
-    host: process.env.HOST || "",
-    database: process.env.DATABASE || "",
-    user: process.env.USER || "",
-    password: process.env.PASSWORD || ""
-}; */
+    export const PORT = process.env.PORT
+    export const DB_PORT = process.env.DB_PORT
+    export const DB_HOST = process.env.DB_HOST
+    export const DB_DATABASE = process.env.DB_DATABASE
+    export const DB_USER = process.env.DB_USER
+    export const DB_PASSWORD = process.env.DB_PASSWORD
