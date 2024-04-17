5f0ba0bd51c01944f52278f7c79e54b5f59ee738
alebig <alebig.pciencia@gmail.com>
2024-04-17 12:06:07 -0300

    Retoque a .env

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@ import { config } from 'dotenv';
 
 config();
 
-//    export const PORT = process.env.DB_HOST || 4038;
-//    export const DB_PORT = process.env.DB_PORT || 4038;
-    export const DB_HOST = process.env.DB_HOST || '127.0.0.1';
-    export const DB_DATABASE = process.env.DB_DATABASE || 'concon';
-    export const DB_USER = process.env.DB_USER || 'alebig';
-    export const DB_PASSWORD = process.env.DB_PASSWORD || 'jab3655';
+    export const PORT = process.env.DB_HOST // 4038;
+    export const DB_PORT = process.env.DB_PORT // 3306;
+    export const DB_HOST = process.env.DB_HOST // localhost/'191.101.235.205';
+    export const DB_DATABASE = process.env.DB_DATABASE // 'concon';
+    export const DB_USER = process.env.DB_USER // 'alebig';
+    export const DB_PASSWORD = process.env.DB_PASSWORD // 'jab3655';
 
 
 //console.log(configu);
